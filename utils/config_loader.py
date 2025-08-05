82246c02db1879582b4bce637036ab6d6e222d9a
Ashutosh Sharma <ashutoshsharmaengg@live.com>
2025-08-05 00:18:58 -0700

    config_loader

---
+++ utils/config_loader.py
@@ -0,0 +1,12 @@
+import yaml
+
+def load_config(config_path: str = "config\config.yaml") ->dict:
+    '''This function will load the config file'''
+    
+    with open(config_path,'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+
+if __name__=='__main__':
+    print(load_config())
