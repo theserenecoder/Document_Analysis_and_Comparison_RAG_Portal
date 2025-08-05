27b584b38cac63b335e9323261ebd06ed21b6c6d
Ashutosh Sharma <ashutoshsharmaengg@live.com>
2025-08-05 00:19:53 -0700

    updated config_loader

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,7 +1,7 @@
 import yaml
 
 def load_config(config_path: str = "config\config.yaml") ->dict:
-    '''This function will load the config file'''
+    '''This function will load the config file (config.yaml)'''
     
     with open(config_path,'r') as file:
         config = yaml.safe_load(file)
