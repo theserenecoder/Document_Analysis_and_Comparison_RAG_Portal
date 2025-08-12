92e835f66e0685b038629552287957ee8273cd3c
Ashutosh Sharma <ashutoshsharmaengg@live.com>
2025-08-12 01:04:36 -0700

    document comparer structure created

--- src/document_compare/document_comparer.py
+++ src/document_compare/document_comparer.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import pandas as pd
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
-from models.model import *
+from model.models import *
 from prompts.prompt_library import PROMPT_REGISTRY
 from utils.model_loader import ModelLoader
 from langchain_core.output_parsers import JsonOutputParser
@@ -17,16 +17,28 @@ class DocumentComparerLLM:
         self.log = CustomLogger().get_logger(__file__)
         self.loader = ModelLoader()
         self.llm = self.loader.load_llm()
-        self.parser
+        self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
+        self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm = self.llm)
+        self.prompt = PROMPT_REGISTRY['document_comparision']
+        self.chain = self.prompt | self.llm | self.parser | self.fixing_parser
+        self.log.info("Document Comparer LLM initialized with model and parser")
     
     def compare_documents(self):
         '''
         Compares two documents and returns a structured comparision.
         '''
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error in compare_documents: {e}")
+            raise DocumentPortalException("An error occured while comparing documents.", sys)
     
     def _format_response(self):
         '''
         Format the response from the LLM into a structured format.
         '''
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error formatting response into DataFrame", error=str(e))
+            raise DocumentPortalException("Error formatting response.",sys)
