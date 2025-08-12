d21340932d565b19e819b9d95b11d65ecdaf58e8
Ashutosh Sharma <ashutoshsharmaengg@live.com>
2025-08-12 01:54:01 -0700

    document comparer data ingestion completed

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -6,20 +6,87 @@ from exception.custom_exception import DocumentPortalException
 
 class DocumentComparer:
     
-    def __init__(self):
-        pass
+    def __init__(self, base_dir):
+        ## Initializing our custom logger
+        self.log = CustomLogger().get_logger(__name__)
+        ## creating path of our directory
+        self.base_dir = Path(base_dir)
+        ## creating a directory if doesn't exists
+        self.base_dir.mkdir(parents=True, exist_ok=True)
     
     def delete_existing_files(self):
         """
         Deletes existing files at the specified paths
         """
-        pass
+        try:
+            ## check if directory path existis and path is a directory
+            if self.base_dir.exists() and self.base_dir.is_dir():
+                ## iterating over files in directory
+                for file in self.base_dir.iterdir():
+                    ## removing/unlining the file
+                    file.unlink()
+                    self.log.info("File deleted.", path=str(file))
+                    
+                self.log.info("Directory cleaned",directory=str(self.base_dir))
+                
+        except Exception as e:
+            self.log.error(f"Error deleting existing files: {e}")
+            raise DocumentPortalException("An error occured while deleting existing files.",sys)
     
-    def save_uploaded_files(self):
+    def save_uploaded_files(self, reference_file, actual_file):
         """
         Save uploaded files to a specified directory
         """
-        pass
+        try:
+            ## deleting any existing file
+            self.delete_existing_files()
+            self.log.info("Existing file deleted successfully")
+            
+            ## updated file
+            ref_path =self.base_dir/reference_file.name
+            ## original file
+            act_path = self.base_dir/reference_file.name
+            
+            ## raise value error if not pf file
+            if not reference_file.name.endswith(".pdf") or not actual_file.name.endswith(".pdf"):
+                raise ValueError("Only PDF files are allowed")
+            
+            ## writing both the files
+            with open(ref_path,'wb') as file:
+                file.write(reference_file.getbuffer())
+                
+            with open(act_path,'wb') as file:
+                file.write(actual_file.getbuffer())
+                
+            self.log.info(f"Files saved.", reference=str(ref_path), actual=str(act_path))
+                
+            
+            
+        except Exception as e:
+            self.log.error(f"Error saving uploaded file: {e}")
+            raise DocumentComparer("An error occured while saving the uploaded file.",sys)
     
-    def read_pdf(self):
-        pass
+    def read_pdf(self, pdf_path:Path)->str:
+        try:
+            ## open the file which has been saved
+            with fitz.open(pdf_path) as doc:
+                ## if file is encrypted raise value error
+                if doc.is_encrypted:
+                    raise ValueError(f"PDF is encrypted: {pdf_path.name}")
+                
+                ## loading text of each page of the document
+                all_text = []
+                for page_num in range(doc.page_count):
+                    page = doc.load_page(page_num)
+                    text = page.get_text()
+                    if text.strip():
+                        all_text.append(f"\n --- Page {page_num + 1} --- \n{text}")
+                
+                
+                self.log.info("PDF read successfully.",file=str(pdf_path), page = len(all_text))
+                
+            return "\n".join(all_text)
+                
+        except Exception as e:
+            self.log.error(f"Error reading files: {e}")
+            raise DocumentComparer("An error ocurred while reading the files.",sys)
