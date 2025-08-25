b2c38e3adfdbfb7898b6f2795a88626cab10e129
Ashutosh Sharma <ashutoshsharmaengg@live.com>
2025-08-24 23:38:32 -0700

    api interface created

--- api/main.py
+++ api/main.py
@@ -3,7 +3,24 @@ from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
+from pathlib import Path
+import os
+
+from src.document_ingestion.data_ingestion import (
+    DocumentHandler, 
+    DocumentComparator, 
+    FaissManager,
+    ChatIngestor
+)
+
+from src.document_analyzer.data_analysis import DocumentAnalyzer
+from src.document_compare.document_comparer import DocumentComparerLLM
+from src.document_chat.retrieval import ConversationRAG
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 app = FastAPI(title = "Document Portal API", version="0.1")
 
@@ -16,8 +33,8 @@ app.add_middleware(
 )
 
 ## serving static and the templates
-app.mount("/static", StaticFiles(directory="../static"), name="static")
-templates = Jinja2Templates(directory="../templates")
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def serve_ui(request : Request):
@@ -27,12 +44,44 @@ async def serve_ui(request : Request):
 @app.get("/health")
 def health() -> Dict[str,str]:
     return {"status":"ok", "service": "document-portal"}
+
+class FastAPIFileAdapter:
+    """Adapt FastAPI UploadFile -> .name + .getbuffer() API"""
     
-    
+    def __init__(self, uf: UploadFile):
+        self._uf = uf
+        self.name = uf.filename
+        
+    def getbuffer(self) -> bytes:
+        self._uf.file.seek(0)
+        return self._uf.file.read()
+
+def _read_pdf_via_handler(handler: DocumentHandler, path:str) ->str:
+    """Utility function to read pdf via DocumentHandler
+    """
+    try:
+        pass
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error reading PDF : {str(e)}")
+
 @app.post("/analyze")
 async def analyze_documents(file: UploadFile = File(...)) -> Any:
     try:
-        pass
+        ## object of document handler
+        dh = DocumentHandler()
+        ## saving the uploaded file
+        save_path = dh.save_pdf(FastAPIFileAdapter(file))
+        ## reading the file content
+        text = _read_pdf_via_handler(dh, save_path)
+        
+        ## object of document analyzer
+        analyzer = DocumentAnalyzer()
+        ## analyzing the document
+        result = analyzer.analyze_document(text)
+        
+        ## returning the response as JSON response
+        return JSONResponse(content=result)
+        
     except HTTPException:
         raise
     except Exception as e:
@@ -42,7 +91,18 @@ async def analyze_documents(file: UploadFile = File(...)) -> Any:
 @app.post("/compare")
 async def compare_documents(reference: UploadFile = File(...), actual: UploadFile = File(...)) -> Any:
     try:
-        pass
+        dc = DocumentComparator()
+        
+        ref_path, act_path = dc.save_uploaded_files(FastAPIFileAdapter(reference), FastAPIFileAdapter(actual))
+        
+        _ = ref_path, act_path
+        combined_test = dc.combine_documents()
+        
+        comp = DocumentComparerLLM()
+        df = comp.compare_documents(combined_test)
+        
+        return {'rows': df.to_dict(orient='records'), "session_id": dc.session_id}
+        
     except HTTPException:
         raise
     except Exception as e:
@@ -50,9 +110,27 @@ async def compare_documents(reference: UploadFile = File(...), actual: UploadFil
     
     
 @app.post("/chat/index")
-async def chat_built_index():
-    try:
-        pass
+async def chat_built_index(
+        files: List[UploadFile] = File(...),
+        session_id: Optional[str] = Form(None),
+        use_session_dirs: bool = Form(True),
+        chunk_size: int = Form(1000),
+        chunk_overlap: int = Form(200),
+        k: int = Form(5)
+) -> Any:
+    try:  
+        wrapped = [FastAPIFileAdapter(f) for f in files]
+        ci = ChatIngestor(
+            temp_base = UPLOAD_BASE,
+            faiss_base = FAISS_BASE,
+            use_session_dirs=use_session_dirs,
+            session_id = session_id or None,
+        )
+        
+        ci.built_retriever(wrapped, chunk_size=chunk_size, chunk_overlap=chunk_overlap, k=k)
+        
+        return {"session_id":ci.session_id, "k":k, "use_session_dirs": use_session_dirs}
+        
     except HTTPException:
         raise
     except Exception as e:
@@ -60,9 +138,33 @@ async def chat_built_index():
     
 
 @app.post("chat/query")
-async def chat_query():
+async def chat_query(
+    question: str = Form(...),
+    session_id: Optional[str]  =Form(None),
+    use_session_dirs: bool = Form(True),
+    k: int = Form(5),
+)->Any:
     try:
-        pass
+        if use_session_dirs and not session_id:
+            raise HTTPException(status_code=400, detail="session_id is required when use_session_dirs is True")
+        
+        ## Prepare FAISS index path
+        index_path = os.path.join(FAISS_BASE, session_id) if use_session_dirs else FAISS_BASE
+        if not os.path.isdir(index_path):
+            raise HTTPException(status_code=400, detail=f"FAISS index not found at {index_path}")
+        
+        ## Initialize LCEL-style RAG pipeline
+        rag = ConversationRAG(session_id=session_id)
+        rag.load_retriever_from_faiss(index_path)
+        
+        response = rag.invoke(question, chat_history=[])
+        
+        return {
+            "answer": response,
+            "session_id":session_id,
+            "k":k,
+            "engine": "LCEL-RAG"
+        }
     except HTTPException:
         raise
     except Exception as e:
---
+++ src/document_chat/retrieval.py
@@ -0,0 +1,140 @@
+import os
+import sys
+from operator import itemgetter
+import streamlit as st
+from typing import Optional, List
+
+from langchain_core.messages import BaseMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.vectorstores import FAISS
+
+from utils.model_loader import ModelLoader
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from prompts.prompt_library import PROMPT_REGISTRY
+from model.models import PromptType
+
+
+class ConversationRAG:
+    
+    def __init__(self, session_id:str, retriever =None):
+        try:
+            self.log = CustomLogger().get_logger(__name__)
+            ## loading session id
+            self.session_id = session_id
+            ## llm
+            self.llm = self._load_llm()
+            ## Prompt
+            self.contextualize_prompt = PROMPT_REGISTRY[PromptType.CONTEXTUALIZE_QUESTION.value]
+            self.qa_prompt = PROMPT_REGISTRY[PromptType.CONTEXT_QA.value]
+            ## loading retriever
+            if retriever is None:
+                raise ValueError("Retriever is None")
+            self.retriever = retriever
+            ## build lcel chain
+            self._build_lcel_chain()
+            
+            self.log.info("ConversationalRAG Initialized", session_id = self.session_id)
+            
+        except Exception as e:
+            self.log.error("Failed to initialised ConversationalRAG", error = str(e))
+            raise DocumentPortalException("Initialization error in ConversationalRAG", sys)
+        
+    def load_retriever_from_faiss(self, index_path:str):
+        """Load a FAISS vectostore from disk and convert to retriever
+        """
+        try:
+            ## load embedding model
+            embedding = ModelLoader().load_embedding_model()
+            ## check if path is a directory path
+            if not os.path.isdir(index_path):
+                raise FileNotFoundError(f"FAISS index directory not found: {index_path}")
+            
+            ## loading index
+            vectore_store = FAISS.load_local(index_path, embedding, allow_dangerous_deserialization=True)
+            self.retriever = vectore_store.as_retriever(search_type ='similarity', search_kwargs={"k":5})
+            self.log.info("Loaded retriever from FAISS index", index_path=index_path)
+            
+            return self.retriever
+            
+        except Exception as e:
+            self.log.error("Failed to load retriever from FAISS", error=str(e))
+            raise DocumentPortalException("Loading error in CoversationalRag",sys)
+        
+    def invoke(self, user_input: str, chat_history:Optional[List[BaseMessage]]=None)->None:
+        """Invoke the retriver
+        """
+        try:
+            chat_history = chat_history or []
+            payload = {"input":user_input, "chat_history":chat_history}
+            answer = self.chain.invoke(payload)
+            
+            if not answer:
+                self.log.warning("No answer generated", user_input = user_input, session_id = self.session_id)
+                return "no answer generated"
+            
+            self.log.info(
+                "Chain invoked successfully",
+                session_id = self.session_id,
+                user_input = user_input,
+                answer_preview = answer[:150]
+            )
+            
+            return answer
+            
+        except Exception as e:
+            self.log.error("Failed to invoke CoversationalRag", error=str(e))
+            raise DocumentPortalException("Invocation error in CoversationalRag",sys)
+        
+    def _load_llm(self):
+        try:
+            ## load llm
+            llm = ModelLoader().load_llm()
+            ## check if llm or not
+            if not llm:
+                raise ValueError("LLM could not be loaded")
+            ## logging
+            self.log.info("LLM loaded successfully", session_id = self.session_id)
+            
+            return llm
+        except Exception as e:
+            self.log.error("Failed to Load llm in CoversationalRag", error=str(e))
+            raise DocumentPortalException("LLM loading error in CoversationalRag",sys)
+    
+    @staticmethod
+    def _format_docs(docs):
+        return "\n\n".join(d.page_content for d in docs)
+        
+    def _build_lcel_chain(self):
+        try:
+            ## Will rewrite question based on our chat history
+            question_rewriter = (
+                {
+                    "input":itemgetter("input"),
+                    "chat_history":itemgetter("chat_history")
+                }
+                | self.contextualize_prompt
+                | self.llm
+                | StrOutputParser() 
+            )
+            ## Retrieve docs for rewriting questions
+            retrieve_docs = question_rewriter | self.retriever | self._format_docs
+            
+            ## feed context + original input + chat history into answer prompt
+            self.chain = (
+                {
+                    "context": retrieve_docs,
+                    "input" : itemgetter("input"),
+                    "chat_history": itemgetter("chat_history")
+                }
+                | self.qa_prompt
+                | self.llm
+                | StrOutputParser()
+            )
+            
+            self.log.info("LECL graph build successfully", session_id = self.session_id)
+            
+        except Exception as e:
+            self.log.error("Failed to build LCEL chain", error=str(e))
+            raise DocumentPortalException("Chain building error in CoversationalRag",sys)
